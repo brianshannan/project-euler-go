56524f90411e86c7410446b9afdc81ac1618ca50
Brian Shannan <brianshannan@gmail.com>
2015-04-16 22:00:21 -0500

    4-5

--- problems/003.go
+++ problems/003.go
@@ -2,63 +2,68 @@ package main
 
 import (
 	"fmt"
-    "math"
+	"math"
 )
 
 func get_primes_list(max_num int) []int {
-    nums := make([]bool, max_num)
-    for i := 0; i < len(nums); i++ {
-        nums[i] = true
-    }
+	nums := make([]bool, max_num)
+	for i := 0; i < len(nums); i++ {
+		nums[i] = true
+	}
 
-    num_primes := 0
-    for i := 2; i < max_num; i++ {
-        if nums[i] == true {
-            num_primes++
-            for j := i*i; j < max_num; j += i {
-                nums[j] = false
-            }
-        }
-    }
+	num_primes := 0
+	for i := 2; i < max_num; i++ {
+		if nums[i] == true {
+			num_primes++
+			for j := i * i; j < max_num; j += i {
+				nums[j] = false
+			}
+		}
+	}
 
-    primes := make([]int, max_num)
-    count := 0
-    for i := 2; i < len(nums); i++ {
-        if nums[i] == true {
-            primes[count] = i
-            count++
-        }
-    }
-    return primes[:count]
+	primes := make([]int, max_num)
+	count := 0
+	for i := 2; i < len(nums); i++ {
+		if nums[i] == true {
+			primes[count] = i
+			count++
+		}
+	}
+	return primes[:count]
 }
 
 func is_prime(num int, primes []int) bool {
-    for i := 0; i < len(primes) && primes[i] < num; i++ {
-        if num % primes[i] == 0 {
-            return false
-        }
-    }
-    return true
+	for i := 0; i < len(primes) && primes[i] < num; i++ {
+		if num%primes[i] == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
-    num := 600851475143
-    sqrt_num := int(math.Sqrt(float64(num))) + 1
-    primes := get_primes_list(sqrt_num)
+	num := 600851475143
+	sqrt_num := int(math.Sqrt(float64(num))) + 1
 
-    // Check factors greater than the square root
-    for i := 2; i < sqrt_num; i++ {
-        if num % i == 0 && is_prime(num / i, primes) {
-            fmt.Println(num / i)
-            return
-        }
-    }
+	// Getting the primes < sqrt_num is sufficient as
+	// if a number n is composite, it must be divisible
+	// by some number m < sqrt(n), so sqrt_num
+	// is the upper bound
+	primes := get_primes_list(sqrt_num)
 
-    // Check factors less than the square root
-    for i := sqrt_num; i > 0; i-- {
-        if num % i == 0 && is_prime(i, primes) {
-            fmt.Println(i)
-            return
-        }
-    }
+	// Check factors greater than the square root
+	for i := 2; i < sqrt_num; i++ {
+		if num%i == 0 && is_prime(num/i, primes) {
+			fmt.Println(num / i)
+			return
+		}
+	}
+
+	// Check factors less than the square root
+	for i := sqrt_num; i > 0; i-- {
+		if num%i == 0 && is_prime(i, primes) {
+			fmt.Println(i)
+			return
+		}
+	}
 }

a88c657b9c62201e94c0db3f967acfedd5b4b199
Brian Shannan <brianshannan@gmail.com>
2015-04-29 13:56:41 -0500

    start on 38

---
+++ src/problems/038.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    // We want to maximize the integer multiplier, so n should be minimized
+    // As n > 1, the minimum is n = 2
+    // As the number of digits equals 9, the most the integer multipler
+    // can have is 4 digits
+    for i := 9999; i > 0; i-- {
+        
+    }
+
+    fmt.Println("TODO")
+}
